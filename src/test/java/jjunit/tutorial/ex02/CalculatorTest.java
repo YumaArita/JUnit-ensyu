da287789295c1ee9af5faa367b52730ca12ba12d
yuma <yuma@yumanoMacBook-Air.local>
2022-02-14 12:18:40 +0900

    second

---
+++ src/test/java/jjunit/tutorial/ex02/CalculatorTest.java
@@ -0,0 +1,19 @@
+package jjunit.tutorial.ex02;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import junit.tutorial.ex01.e02.Calculator;
+
+public class CalculatorTest {
+	
+	@Test
+	@DisplayName("1-2")
+	void test2() {
+		Calculator CU = new Calculator();
+		assertThrows(IllegalArgumentException.class,() -> {CU.divide(3, 0);});
+		
+	}
+}
--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -1,7 +1,21 @@
 package junit.tutorial.ex01.e01;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class StringUtilsTest {
+
+	@Test
+	@DisplayName("1-1")
+	void test1() {
+
+//		 StringUtils SU = new StringUtils();
+		assertEquals("aaa", StringUtils.toSnakeCase("aaa"));
+		assertEquals("hello_world", StringUtils.toSnakeCase("HelloWorld"));
+		assertEquals("practice_junit", StringUtils.toSnakeCase("practiceJunit"));
+	}
+	
 
 }
